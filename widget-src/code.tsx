d27b12b9c9bdade25d8cec0f874b37659bb37d66
sei.shinkuma <shinkuma.sei@gmail.com>
2025-07-05 19:36:10 +0900

    計算できるようにした

--- widget-src/code.tsx
+++ widget-src/code.tsx
@@ -84,6 +84,27 @@ function PointWidget() {
 }
 
 function CounterWidget() {
+  const [total, setTotal] = useSyncedState('total', 0)
+
+  const calculateTotal = () => {
+    // 選択されたすべてのノードについてStickyノートを収集
+    const selection = figma.currentPage.selection;
+    const pointWidgets: WidgetNode[] = [];
+    selection.forEach(node => {
+      pointWidgets.push(...getPointWidgets(node));
+    });
+
+    const sum = pointWidgets.map(widget => {
+      const count = widget.widgetSyncedState['count']
+      if (typeof count === 'number') {
+        return count
+      }
+      return 0
+    }).reduce((a, b) => a + b, 0);
+
+    setTotal(sum)
+  }
+
   return (
     <AutoLayout
       verticalAlignItems={'center'}
@@ -94,9 +115,34 @@ function CounterWidget() {
       stroke={'#E6E6E6'}
     >
       <Text fontSize={24}>Total:</Text>
-      <Text fontSize={32}>10</Text>
+      <Text fontSize={32}>{total}</Text>
+      <SVG
+        src={`<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M17.65 6.35C16.2 4.9 14.21 4 12 4C7.58 4 4.01 7.58 4.01 12C4.01 16.42 7.58 20 12 20C15.73 20 18.84 17.45 19.73 14H17.65C16.83 16.33 14.61 18 12 18C8.69 18 6 15.31 6 12C6 8.69 8.69 6 12 6C13.66 6 15.14 6.69 16.22 7.78L13 11H20V4L17.65 6.35Z" fill="black"/>
+        </svg>`}
+        onClick={calculateTotal}
+      />
     </AutoLayout>
   )
 }
 
 widget.register(Widget)
+
+// Stickyノートを収集する関数（再帰的に探索）
+function getPointWidgets(node: SceneNode): WidgetNode[] {
+  let pointWidgets: WidgetNode[] = [];
+
+  // セクションの場合、その中のすべての子要素を再帰的に処理
+  if (node.type === 'SECTION') {
+    node.children.forEach(child => {
+      pointWidgets = pointWidgets.concat(getPointWidgets(child));
+    });
+  }
+
+  // Stickyノートの場合、stickies配列に追加
+  if (node.type === 'WIDGET' && node.widgetId === figma.widgetId && node.widgetSyncedState['widgetType'] === 'point') {
+    pointWidgets.push(node);
+  }
+
+  return pointWidgets;
+}
