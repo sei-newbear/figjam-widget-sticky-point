5a61a37f0de79db26ae8eaf1f88870ac26f5c77f
sei.shinkuma <shinkuma.sei@gmail.com>
2025-07-05 19:50:50 +0900

    デザイン調整

--- widget-src/code.tsx
+++ widget-src/code.tsx
@@ -40,22 +40,12 @@ function PointWidget() {
   return (
     <AutoLayout
       verticalAlignItems={'center'}
-      spacing={8}
-      padding={16}
+      // spacing={8}
+      padding={8}
       cornerRadius={8}
       fill={'#FFFFFF'}
       stroke={'#E6E6E6'}
     >
-      <SVG
-        src={`<svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <rect width="30" height="30" rx="15" fill="white"/>
-        <rect x="7.5" y="14.0625" width="15" height="1.875" fill="black" fill-opacity="0.8"/>
-        <rect x="0.5" y="0.5" width="29" height="29" rx="14.5" stroke="black" stroke-opacity="0.1"/>
-        </svg>`}
-        onClick={() => {
-          setCount(count - 1)
-        }}
-      ></SVG>
       <Input
         value={String(count)}
         placeholder="0"
@@ -65,20 +55,10 @@ function PointWidget() {
             setCount(newValue)
           }
         }}
-        fontSize={32}
-        width={60}
+        fontSize={24}
+        width={64}
         horizontalAlignText={'center'}
       />
-      <SVG
-        src={`<svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <rect width="30" height="30" rx="15" fill="white"/>
-        <path d="M15.9375 7.5H14.0625V14.0625H7.5V15.9375H14.0625V22.5H15.9375V15.9375H22.5V14.0625H15.9375V7.5Z" fill="black" fill-opacity="0.8"/>
-        <rect x="0.5" y="0.5" width="29" height="29" rx="14.5" stroke="black" stroke-opacity="0.1"/>
-        </svg>`}
-        onClick={() => {
-          setCount(count + 1)
-        }}
-      ></SVG>
     </AutoLayout>
   )
 }
