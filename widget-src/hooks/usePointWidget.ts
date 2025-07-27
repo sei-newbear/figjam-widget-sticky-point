fd06d530021f539e5b1d497dff0e06e7455984b9
sei.shinkuma <shinkuma.sei@gmail.com>
2025-07-27 21:08:21 +0900

    グループ化・解除処理を修正

--- widget-src/hooks/usePointWidget.ts
+++ widget-src/hooks/usePointWidget.ts
@@ -5,10 +5,34 @@ const { useSyncedState, useStickable, useWidgetNodeId, useEffect } = widget
 export function usePointWidget(groupingEnabled: boolean) {
   const [beforeGroupingEnabled, setBeforeGroupingEnabled] = useSyncedState<boolean>('beforeGroupingEnabled', false)
   const widgetNodId = useWidgetNodeId()
-  function unGroupNode(node: BaseNode) {
-    if (node.parent?.type === "GROUP") {
-      figma.ungroup(node.parent);
+  async function getWidgetNode(): Promise<WidgetNode | null> {
+    const node = await figma.getNodeByIdAsync(widgetNodId);
+    if(node?.type === "WIDGET"){
+      return node
     }
+
+    return null;
+  }
+  function unGroupNode(node: BaseNode) {
+    const parent = node.parent
+    
+    if (parent?.type !== "GROUP") return;
+    // グループの子要素がウィジェットとそれ以外のNodeが1つずつではない場合のみグループを解除する
+    // グループの子要素が3つ以上ある場合、この機能でグループ化したのか、元々グループ化していたのか判断つかないため、解除せずそのままにする
+    if (parent.children.length !== 2) return;
+
+    figma.ungroup(parent);
+  }
+
+  function groupWidgetNode(widgetNode: WidgetNode) {
+    const widgetParent = widgetNode.parent
+    if(!widgetParent) return
+    if(widgetParent.type === "GROUP") return;
+    
+    const widgetStuckTo = widgetNode.stuckTo
+    if(!widgetStuckTo) return
+
+    figma.group([widgetNode, widgetStuckTo], widgetNode.parent);
   }
 
   useStickable(async (e: WidgetStuckEvent) => {     
@@ -17,7 +41,7 @@ export function usePointWidget(groupingEnabled: boolean) {
     }
 
     async function handleOldHost(oldHostId: string) {
-      const widgetNode = await getNode(widgetNodId);
+      const widgetNode = await getWidgetNode();
       if (!widgetNode) return;
 
       const oldHost = await getNode(oldHostId);
@@ -34,14 +58,9 @@ export function usePointWidget(groupingEnabled: boolean) {
     async function handleNewHost(newHostId: string) {
       if (!groupingEnabled) return;
 
-      const newHost = await getNode(newHostId);
-      const widgetNode = await getNode(widgetNodId);
-                  
-      if (newHost && widgetNode) {
-        const newHostParent = newHost.parent;
-        if (newHostParent) {
-          figma.group([newHost, widgetNode], newHostParent);
-        }
+      const widgetNode = await getWidgetNode();                  
+      if (widgetNode) {
+        groupWidgetNode(widgetNode);
       }
     }
 
@@ -58,14 +77,14 @@ export function usePointWidget(groupingEnabled: boolean) {
     setBeforeGroupingEnabled(groupingEnabled)
 
     if(groupingEnabled){
-      figma.getNodeByIdAsync(widgetNodId).then(node => {
-        if(node?.type === 'WIDGET' && node.stuckTo && node.parent){
-          figma.group([node.stuckTo, node], node.parent);
+      getWidgetNode().then(node => {
+        if(node){
+          groupWidgetNode(node);
         }
       })
     } else {
-      figma.getNodeByIdAsync(widgetNodId).then(node => {
-        if(node?.parent){
+      getWidgetNode().then(node => {
+        if(node){
           unGroupNode(node);
         }
       })
